50b56ce5ccf21722f05fd52ef7c9b7fd5e6f943f
afarica <57631090+afarica@users.noreply.github.com>
2019-11-12 05:51:00 +0600

    Update p20.py

--- p20.py
+++ p20.py
@@ -1,3 +1,4 @@
+# Given two integers A and B (На английском языке что бы Вы научились понимать A ≤ B). Print all numbers from A to B inclusively.
 a=int(input("Enter the 1 number:"))
 b=int(input("Enter the 2 number:"))
 for x in range(a,b+1):
